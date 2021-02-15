a7eba520da8e6e8e945cecd4ac94240362b69d8f
Jonathan <jonathan.max.annett@gmail.com>
2021-02-16 09:07:44 +1100

    added wrapper, credit - https://davidwalsh.name/vendor-prefix

--- code/vendor-prefix.js
+++ code/vendor-prefix.js
@@ -1,5 +1,11 @@
-//attribution: https://davidwalsh.name/vendor-prefix
-var prefix = (function () {
+//based loosely on: https://davidwalsh.name/vendor-prefix
+  
+(function(){
+ if (typeof window!=='object') 
+    return;
+ if (typeof window.vendorPrefix!=='undefined') 
+    return;
+  
   var styles = window.getComputedStyle(document.documentElement, ''),
     pre = (Array.prototype.slice
       .call(styles)
@@ -7,7 +13,7 @@ var prefix = (function () {
       .match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o'])
     )[1],
     dom = ('WebKit|Moz|MS|O').match(new RegExp('(' + pre + ')', 'i'))[1];
-  return {
+  window.vendorPrefix = {
     dom: dom,
     lowercase: pre,
     css: '-' + pre + '-',
