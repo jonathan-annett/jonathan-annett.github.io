392210a3d0d18ba4358b3bbec44bbadf0e138368
Jonathan <jonathan.max.annett@gmail.com>
2021-06-10 21:01:29 +1000

    Delete zed/app/js/zed directory

--- zed/app/js/zed/log.js
+++
@@ -1,9 +0,0 @@
-/* global define */
-define(function(require, exports, module) {
-    return {
-        log: function(level, args) {
-            console[level].apply(console, ["[Sandbox]"].concat(args));
-            return Promise.resolve();
-        }
-    };
-});
--- zed/app/js/zed/preview.js
+++
@@ -1,9 +0,0 @@
-/* global define, zed */
-define(function(require, exports, module) {
-    return {
-        showPreview: function(html, open) {
-            zed.getService("preview").showPreview(html, open);
-            return Promise.resolve();
-        }
-    };
-});
