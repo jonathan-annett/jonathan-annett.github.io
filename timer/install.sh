c506da1e3655d7b58dce52c8a3b5f2f895a5d98c
Jonathan <jonathan.max.annett@gmail.com>
2023-03-17 18:40:07 +1100

    Create install.sh

---
+++ timer/install.sh
@@ -0,0 +1,10 @@
+wget  https://jonathan-annett.github.io/timer/timer.html
+wget  https://jonathan-annett.github.io/timer/timer.js
+wget  https://jonathan-annett.github.io/timer/timer.css
+wget  https://jonathan-annett.github.io/timer/fsapi.js
+wget  https://jonathan-annett.github.io/timer/index.js
+wget  https://jonathan-annett.github.io/timer/timer.html
+wget  https://jonathan-annett.github.io/timer/keyhelp.js
+wget  https://jonathan-annett.github.io/timer/keyhelp.css
+
+
