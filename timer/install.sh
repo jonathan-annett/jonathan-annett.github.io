788198409d8dbbe4988620b04d493b3c2ef35200
Jonathan <jonathan.max.annett@gmail.com>
2023-03-17 19:08:06 +1100

    Update install.sh

--- timer/install.sh
+++ timer/install.sh
@@ -1,15 +1,15 @@
 # script to download timer app to openwrt router
 mkdir -p /root/timer
 cd /root/timer
-wget  https://jonathan-annett.github.io/timer/timer.html
-wget  https://jonathan-annett.github.io/timer/timer.js
-wget  https://jonathan-annett.github.io/timer/timer.css
-wget  https://jonathan-annett.github.io/timer/fsapi.js
-wget  https://jonathan-annett.github.io/timer/index.js
-wget  https://jonathan-annett.github.io/timer/index.html
-wget  https://jonathan-annett.github.io/timer/keyhelp.js
-wget  https://jonathan-annett.github.io/timer/keyhelp.css
+wget  https://jonathan-annett.github.io/timer/timer.html  -O ./timer.html
+wget  https://jonathan-annett.github.io/timer/timer.js    -O ./timer.js
+wget  https://jonathan-annett.github.io/timer/timer.css   -O ./timer.css
+wget  https://jonathan-annett.github.io/timer/fsapi.js    -O ./fsapi.js
+wget  https://jonathan-annett.github.io/timer/index.js    -O ./index.js
+wget  https://jonathan-annett.github.io/timer/index.html  -O ./index.html
+wget  https://jonathan-annett.github.io/timer/keyhelp.js  -O ./keyhelp.js
+wget  https://jonathan-annett.github.io/timer/keyhelp.css -O ./keyhelp.css
 cd /www
-test -e ./timer && rm ,/timer
-ln -s /root/timer .
+test -e ./timer && rm ./timer
+ln -s /root/timer ./timer
 cd /root
