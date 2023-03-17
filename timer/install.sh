5242f20cd434437dfa2ccd79a7d2a33db7ac2e60
Jonathan <jonathan.max.annett@gmail.com>
2023-03-17 19:02:00 +1100

    Update install.sh

--- timer/install.sh
+++ timer/install.sh
@@ -1,3 +1,5 @@
+mkdir -p /root/timer
+cd /root/timer
 wget  https://jonathan-annett.github.io/timer/timer.html
 wget  https://jonathan-annett.github.io/timer/timer.js
 wget  https://jonathan-annett.github.io/timer/timer.css
@@ -6,5 +8,7 @@ wget  https://jonathan-annett.github.io/timer/index.js
 wget  https://jonathan-annett.github.io/timer/index.html
 wget  https://jonathan-annett.github.io/timer/keyhelp.js
 wget  https://jonathan-annett.github.io/timer/keyhelp.css
-
-
+cd /www
+test -e ./timer && rm ,/timer
+ln -s /root/timer .
+cd /root
