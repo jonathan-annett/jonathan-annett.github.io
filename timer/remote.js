5422bc9ce75afd19f2e2ed1d4d1291458949a922
Jonathan <jonathan.max.annett@gmail.com>
2023-04-23 14:54:26 +1000

    wip

--- timer/remote.js
+++ timer/remote.js
@@ -15,9 +15,48 @@ if ( location.search.startsWith('?') && location.search.length===25) {
 const own_id = localStorage.getItem('own_id') || '';
 const peer_id = localStorage.getItem('peer_id') || '';
 
-const own_id_ = document.querySelector('#own_id');
-const peer_id_ = document.querySelector('#peer_id');
+const own_id_    = document.querySelector('#own_id');
+const peer_id_   = document.querySelector('#peer_id');
+const timer_btn_ = document.querySelector('#tier_btn');
+const control_btn_ = document.querySelector('#control_btn');
 
 own_id_.innerHTML = own_id;
-peer_id.innerHTML = peer_id;
+peer_id_.innerHTML = peer_id;
+let timerWin, controlWin;
 
+timer_btn_.onclick = function(e) {
+    openTimerWindow(!!timerWin);
+};
+
+
+function openTimerWindow(close) {
+    if (close === true) {
+        if (timerWin) timerWin.close();
+        timerWin = undefined;
+    } else {
+        timerWin = open("timer.html?presenter", 'remote_timer_window', "location=0");
+        if (timerWin) timerWin.addEventListener("unload", onTimerWinUnload);
+
+    }
+    return false;
+}
+
+function onTimerWinUnload() {
+    timerWin = undefined;
+}
+
+function openControllerWindow(close) {
+    if (close === true) {
+        if (controlWin) controlWin.close();
+        controlWin = undefined;
+    } else {
+        controlWin = open("timer.html", 'remote_control_window', "location=0");
+        if (controlWin) controlWin.addEventListener("unload", onControlWinUnload);
+
+    }
+    return false;
+}
+
+function onControlWinUnload() {
+    controlWin = undefined;
+}
