f7aac3b00cd1b28dd259245f1e1e4cb01ecf92ec
Jonathan <jonathan.max.annett@gmail.com>
2024-06-27 17:44:03 +1000

    ...

--- mcec/speech/audioCaptureTest.js
+++ mcec/speech/audioCaptureTest.js
@@ -4,12 +4,12 @@ audioCapturePeer.init();
 pptLink();
 
 
-document.onabort('PPTSpeechEvent',function(e){  
+document.addEventListener('PPTSpeechEvent',function(e){  
     document.getElementById('transcription').textContent = e.detail.interim_transcript;
 });
 
 
-document.onabort('PPTSpeechEvent',function(e){
+document.addEventListener('PPTSpeechEvent',function(e){
     document.getElementById('PPT_transcription').textContent =  e.detail.captions;
 });
 
