0170e192ed2a8d193029ae37dab57d4bcc42cdc3
Jonathan <jonathan.max.annett@gmail.com>
2024-06-27 11:53:34 +1000

    Create speechProcessor.js

---
+++ mcec/speech/speechProcessor.js
@@ -0,0 +1,30 @@
+class SpeechProcessor extends AudioWorkletProcessor {
+    constructor() {
+        super();
+        this.recognition = new (self.SpeechRecognition || self.webkitSpeechRecognition)();
+        this.recognition.lang = 'en-US';
+        this.recognition.continuous = true;
+
+        this.recognition.onresult = (event) => {
+            const transcript = Array.from(event.results)
+                .map(result => result[0])
+                .map(result => result.transcript)
+                .join('');
+            this.port.postMessage(transcript);
+        };
+
+        this.recognition.onerror = (event) => {
+            console.error(event.error);
+        };
+
+        this.recognition.start();
+    }
+
+    process(inputs, outputs, parameters) {
+        // Audio processing is done automatically by the Speech API,
+        // so we don't need to process audio samples here.
+        return true;
+    }
+}
+
+registerProcessor('speech-processor', SpeechProcessor);
