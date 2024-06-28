60e58dedf2e5e17b8caad4471141dceb5ab25e9b
Jonathan <jonathan.max.annett@gmail.com>
2024-06-28 10:41:17 +1000

    fixed some startup issues' with web comp

--- mcec/speech/GoogleSpeechAPI_SPN.js
+++ mcec/speech/GoogleSpeechAPI_SPN.js
@@ -1,200 +1,182 @@
- 
+class GoogleSpeechAPI_SPN extends HTMLElement {
+    constructor() {
+        super();
 
-	 class GoogleSpeechAPI_SPN extends HTMLElement {
-		constructor() {
-			super();
-	
-			// Attach a shadow DOM tree to the instance
-			this.attachShadow({ mode: 'open' });
-	
-			// Create elements and attach them to the shadow DOM
-			const wrapper = document.createElement('div');
-			wrapper.setAttribute('class', 'wrapper');
-			this.shadowRoot.appendChild(wrapper);
-	
-			// Add some basic styles
-			const style = document.createElement('style');
-			style.textContent = `
-				.wrapper {
-					border: 1px solid #ccc;
-					padding: 10px;
-					margin: 10px;
-					background-color: #f9f9f9;
-				}
-			`;
-			this.shadowRoot.appendChild(style);
+        // Attach a shadow DOM tree to the instance
+        this.attachShadow({ mode: 'open' });
 
+        // Create elements and attach them to the shadow DOM
+        const wrapper = document.createElement('div');
+        wrapper.setAttribute('class', 'wrapper');
+        this.shadowRoot.appendChild(wrapper);
 
-		}
+        // Add some basic styles
+        const style = document.createElement('style');
+        style.textContent = `
+            .wrapper {
+                border: 1px solid #ccc;
+                padding: 10px;
+                margin: 10px;
+                background-color: #f9f9f9;
+            }
+        `;
+        this.shadowRoot.appendChild(style);
+    }
 
-		connectedCallback() {
-			this.init();
-			this.render();
-		}
-	
-	
-		// Getter and Setter for attrib1
-		get language() {
-			return this.getAttribute('language');
-		}
-	
-		set language(value) {
-			this.setAttribute('language', value);
-		}
-	
-		// Getter and Setter for transcript
-		get transcript() {
-			return this.getAttribute('transcript');
-		}
-	
-		set transcript(value) {
-			this.setAttribute('transcript', value);
-		}
+    connectedCallback() {
+        this.init();
+        this.render();
+    }
 
-		// Getter and Setter for interimTranscript
-		get interimTranscript() {
-			return this.getAttribute('interim-transcript');
-		}
-	
-		set interimTranscript(value) {
-			this.setAttribute('interim-transcript', value);
-		}
-	
-		// Observed attributes
-		static get observedAttributes() {
-			return ['transcript','interim-transcript'];
-		}
-	
-		// Callback when observed attributes change
-		attributeChangedCallback(name, oldValue, newValue) {
-			if (oldValue !== newValue) {
-				this.render();
-			}
-		}
-	
-		// Initial rendering
-		connectedCallback() {
-			this.render();
-		}
-	
-		// Rendering logic
-		render() {
-			this.shadowRoot.querySelector('.wrapper').textContent = `${this.transcript} ${this.interimTranscript}`;
-		}
+    // Getter and Setter for language
+    get language() {
+        return this.getAttribute('language');
+    }
 
-		init () {
-			var recognition = new webkitSpeechRecognition();
-			this.recognition= recognition;
+    set language(value) {
+        this.setAttribute('language', value);
+    }
 
-			var recognizing = false;
-			var ignore_onend = false;
-			var final_transcript = '';
-			var full_transcript = [
-				final_transcript,''
-			];
+    // Getter and Setter for transcript
+    get transcript() {
+        return this.getAttribute('transcript');
+    }
 
-			 
-			var start_timestamp ;
+    set transcript(value) {
+        this.setAttribute('transcript', value);
+    }
 
-			recognition.continuous = true;
-			recognition.interimResults = true;
+    // Getter and Setter for interimTranscript
+    get interimTranscript() {
+        return this.getAttribute('interim-transcript');
+    }
 
-			function showInfo(x) {
-				this.shadowRoot.querySelector('.wrapper').className = "wrapper "+x;
-			}
+    set interimTranscript(value) {
+        this.setAttribute('interim-transcript', value);
+    }
 
-			recognition.onstart = function() {
-			  recognizing = true;
-			  showInfo('info_speak_now');
-			};
-		  
-			recognition.onerror = function(event) {
-			  if (event.error == 'no-speech') {
-				showInfo('info_no_speech');
-				ignore_onend = true;
-			  }
-			  if (event.error == 'audio-capture') {
-				showInfo('info_no_microphone');
-				ignore_onend = true;
-			  }
-			  if (event.error == 'not-allowed') {
-				if (event.timeStamp - start_timestamp < 100) {
-				  showInfo('info_blocked');
-				} else {
-				  showInfo('info_denied');
-				}
-				ignore_onend = true;
-			  }
-			};
-		  
-			recognition.onend = function() {
-			  recognizing = false;
-			  if (ignore_onend) {
-				return;
-			  }
-			  if (!final_transcript) {
-				showInfo('info_start');
-				return;
-			  }
-			 
-			};
-		  
-			recognition.onresult = function(event) {
+    // Observed attributes
+    static get observedAttributes() {
+        return ['transcript', 'interim-transcript'];
+    }
 
-			  var interim_transcript = '';
+    // Callback when observed attributes change
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (oldValue !== newValue) {
+            this.render();
+        }
+    }
 
-			  if (typeof(event.results) == 'undefined') {
-				recognition.onend = null;
-				recognition.stop();
-				 return;
-			  } 
+    // Rendering logic
+    render() {
+        this.shadowRoot.querySelector('.wrapper').textContent = `${this.transcript} ${this.interimTranscript}`;
+    }
 
-			  for (var i = event.resultIndex; i < event.results.length; ++i) {
-				if (event.results[i].isFinal) {
-				  final_transcript += event.results[i][0].transcript;
-				  this.transcript  = final_transcript;
-				} else {
-				  interim_transcript += event.results[i][0].transcript;
-				  this.interimTranscript  = interim_transcript; 
-				}
-			  }
+    init() {
+        const recognition = new webkitSpeechRecognition();
+        this.recognition = recognition;
 
-			  full_transcript[full_transcript.length-2] = final_transcript;
-			  full_transcript[full_transcript.length-1] = interim_transcript;
-			  
-			  const customEvent = new CustomEvent('CustomSpeechEvent', { detail: {
-					provider : "google-spn",
-					transcript : full_transcript.join("\n")} 
-				});
-			  document.dispatchEvent(customEvent);    
-			  
-			};
+        let recognizing = false;
+        let ignore_onend = false;
+        let final_transcript = '';
+        const full_transcript = [final_transcript, ''];
 
-			this.start = function () {
-					
-				if (recognizing) {
-					recognition.stop();
-					return;
-				  }
-				final_transcript = '';
-				full_transcript [ full_transcript.length-1]=final_transcript;
-				full_transcript.push('');
+        let start_timestamp;
 
-				while (full_transcript.length > 2 && full_transcript[0]==='') {
-					full_transcript.splice(0,1);
-				}
+        recognition.continuous = true;
+        recognition.interimResults = true;
 
-				recognition.lang = "10";//select_dialect.value;
-				recognition.start();
-				ignore_onend = false;
-				showInfo('info_allow');
-				start_timestamp = event.timeStamp;
-		  }
+        const showInfo = (x) => {
+            this.shadowRoot.querySelector('.wrapper').className = `wrapper ${x}`;
+        };
 
-		}
-	}
-	
-	// Define the new element
-	customElements.define('google-speech-spn', GoogleSpeechAPI_SPN);
-	
-     
+        recognition.onstart = () => {
+            recognizing = true;
+            showInfo('info_speak_now');
+        };
+
+        recognition.onerror = (event) => {
+            if (event.error === 'no-speech') {
+                showInfo('info_no_speech');
+                ignore_onend = true;
+            }
+            if (event.error === 'audio-capture') {
+                showInfo('info_no_microphone');
+                ignore_onend = true;
+            }
+            if (event.error === 'not-allowed') {
+                if (event.timeStamp - start_timestamp < 100) {
+                    showInfo('info_blocked');
+                } else {
+                    showInfo('info_denied');
+                }
+                ignore_onend = true;
+            }
+        };
+
+        recognition.onend = () => {
+            recognizing = false;
+            if (ignore_onend) {
+                return;
+            }
+            if (!final_transcript) {
+                showInfo('info_start');
+                return;
+            }
+        };
+
+        recognition.onresult = (event) => {
+            let interim_transcript = '';
+
+            if (typeof event.results === 'undefined') {
+                recognition.onend = null;
+                recognition.stop();
+                return;
+            }
+
+            for (let i = event.resultIndex; i < event.results.length; ++i) {
+                if (event.results[i].isFinal) {
+                    final_transcript += event.results[i][0].transcript;
+                    this.transcript = final_transcript;
+                } else {
+                    interim_transcript += event.results[i][0].transcript;
+                    this.interimTranscript = interim_transcript;
+                }
+            }
+
+            full_transcript[full_transcript.length - 2] = final_transcript;
+            full_transcript[full_transcript.length - 1] = interim_transcript;
+
+            const customEvent = new CustomEvent('CustomSpeechEvent', {
+                detail: {
+                    provider: 'google-spn',
+                    transcript: full_transcript.join('\n')
+                }
+            });
+            document.dispatchEvent(customEvent);
+        };
+
+        this.start = () => {
+            if (recognizing) {
+                recognition.stop();
+                return;
+            }
+            final_transcript = '';
+            full_transcript[full_transcript.length - 1] = final_transcript;
+            full_transcript.push('');
+
+            while (full_transcript.length > 2 && full_transcript[0] === '') {
+                full_transcript.splice(0, 1);
+            }
+
+            recognition.lang = this.language || 'en-AU';
+            recognition.start();
+            ignore_onend = false;
+            showInfo('info_allow');
+            start_timestamp = event.timeStamp;
+        };
+    }
+}
+
+// Define the new element
+customElements.define('google-speech-spn', GoogleSpeechAPI_SPN);
