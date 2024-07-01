1c22963d3b4fda2ea55737204273d0b6dce71215
Jonathan <jonathan.max.annett@gmail.com>
2024-07-01 10:52:05 +1000

    removed metering for now

--- mcec/speech/audioCaptureTest.js
+++ mcec/speech/audioCaptureTest.js
@@ -9,31 +9,4 @@ document.addEventListener('PPTSpeechEvent',function(e){
 });
 */
 
-var audioContext;
-
-const startAudio = async (context, meterElement) => {
-    await context.audioWorklet.addModule('volume-meter-processor.js');
-    const mediaStream = await navigator.mediaDevices.getUserMedia({audio: true});
-    const micNode = context.createMediaStreamSource(mediaStream);
-    const volumeMeterNode = new AudioWorkletNode(context, 'volume-meter');   
-    volumeMeterNode.port.onmessage = ({data}) => {
-      meterElement.value = data * 500;
-    };
-    micNode.connect(volumeMeterNode).connect(context.destination);
-  };
-document.addEventListener("DOMContentLoaded",function(){
-
-    const buttonEl = document.getElementById('button-start');
-    const meterEl = document.getElementById('volume-meter');
-    buttonEl.disabled = false;
-    meterEl.disabled = false;
-    buttonEl.addEventListener('click', async () => {
-        audioContext = new AudioContext();
-
-        await startAudio(audioContext, meterEl);
-        audioContext.resume();
-        buttonEl.disabled = true;
-        //buttonEl.textContent = 'Stop Meter';
-    }, false);
-
-});
+ 
