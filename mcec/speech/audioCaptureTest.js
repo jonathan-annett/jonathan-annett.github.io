b4e826101ca02f2bf6f43c531f783cd02dfa09fc
Jonathan <jonathan.max.annett@gmail.com>
2024-07-01 13:04:05 +1000

    push html changes

--- mcec/speech/audioCaptureTest.js
+++ mcec/speech/audioCaptureTest.js
@@ -9,4 +9,14 @@ document.addEventListener('PPTSpeechEvent',function(e){
 });
 */
 
- 
+function applyStyles() {
+    const font = document.getElementById('fonts').value;
+    const fontSize = document.getElementById('fontSize').value + 'px';
+    const pageColor = document.getElementById('pageColor').value;
+    const textColor = document.getElementById('textColor').value;
+
+    document.documentElement.style.setProperty('--font-family', font);
+    document.documentElement.style.setProperty('--font-size', fontSize);
+    document.documentElement.style.setProperty('--page-color', pageColor);
+    document.documentElement.style.setProperty('--text-color', textColor);
+}
