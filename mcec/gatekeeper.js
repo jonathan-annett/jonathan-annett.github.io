b64cff4faa495dde42f48547bd26ee50b93e3cf8
Jonathan <jonathan.max.annett@gmail.com>
2024-07-02 14:16:17 +1000

    ...

--- mcec/gatekeeper.js
+++ mcec/gatekeeper.js
@@ -89,12 +89,19 @@ document.addEventListener('DOMContentLoaded',function(){
             // remove the styling that hides gatekept elements
             gatekeeperStyle.textContent = `
 
+             html, body {
+                height: 100%;
+                margin: 0;
+                padding: 0;
+            }
+ 
             iframe.gatekept {
                 position: absolute;
-                left:0;
-                right:0;
-                top:0;
-                bottom:0;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                border: none;
             }
 
             .gatekeeper {
