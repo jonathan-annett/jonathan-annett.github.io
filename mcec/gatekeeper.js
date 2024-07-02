5ea173a403ee195a2d8232cefa2fca5bc4d35a5d
Jonathan <jonathan.max.annett@gmail.com>
2024-07-02 13:38:27 +1000

    update

--- mcec/gatekeeper.js
+++ mcec/gatekeeper.js
@@ -72,6 +72,7 @@ document.addEventListener('DOMContentLoaded',function(){
                         }
                     });
                     document.dispatchEvent(customEvent);
+                    console.log("This Browser Key:",key);
                     return;
                 }
             }
