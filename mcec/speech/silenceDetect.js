6cf4d7aaab5194e0d1d29cb5f8a07085b604c54f
Jonathan <jonathan.max.annett@gmail.com>
2024-07-31 10:57:27 +1000

    test

--- mcec/speech/silenceDetect.js
+++ mcec/speech/silenceDetect.js
@@ -1,107 +1,111 @@
+//jshint esnext : false
+//jshint esversion : 8
+//jshint undef : true
 
 class SilenceDetector {
-    constructor(threshold = 0.01, silenceDuration = 5000, renotifyTimeout = 500, getEnergyFunction=null,target) {
-        this.eventTarget = (target !== window && target) || null;
-        this.threshold = threshold;
-        this.thresholdWeight = 256;
-        this.silenceDuration = silenceDuration;
-        this.silenceTimeout = null;
-        this.isSilent = false;
-        this.renotifyTimeout = renotifyTimeout;
-        this.lastAudioNotified = Date.now();
-        if (typeof getEnergyFunction==='function') {
-            this.getEnergy = getEnergyFunction;
-            this.monitor();
-        } else {
-            this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
-            this.getEnergy = this._getEnergy;
-            this.init();
-        }
-    }
+	constructor(threshold = 0.01, silenceDuration = 5000, renotifyTimeout = 500, getEnergyFunction=null,target=null) {
+		this.eventTarget = (target !== window && target) || null;
+		this.threshold = threshold;
+		this.thresholdWeight = 256;
+		this.silenceDuration = silenceDuration;
+		this.silenceTimeout = null;
+		this.isSilent = false;
+		this.renotifyTimeout = renotifyTimeout;
+		this.lastAudioNotified = Date.now();
+		if (typeof getEnergyFunction==='function') {
+			this.getEnergy = getEnergyFunction;
+			this.monitor();
+		} else {
+		    const AC = window.AudioContext || window.webkitAudioContext;
+			this.audioContext = new AC();
+			this.getEnergy = this._getEnergy;
+			this.init();
+		}
+	}
 
-    async init() {
-        try {
-            const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-            this.audioInput = this.audioContext.createMediaStreamSource(stream);
-            this.analyser = this.audioContext.createAnalyser();
-            this.analyser.fftSize = 256;
-            this.dataArray = new Uint8Array(this.analyser.frequencyBinCount);
-            this.audioInput.connect(this.analyser);
-            this.monitor();
-        } catch (error) {
-            console.error('Error accessing audio input:', error);
-        }
-    }
+	async init() {
+		try {
+			const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+			this.audioInput = this.audioContext.createMediaStreamSource(stream);
+			this.analyser = this.audioContext.createAnalyser();
+			this.analyser.fftSize = 256;
+			this.dataArray = new Uint8Array(this.analyser.frequencyBinCount);
+			this.audioInput.connect(this.analyser);
+			this.monitor();
+		} catch (error) {
+			console.error('Error accessing audio input:', error);
+		}
+	}
 
-    _getEnergy () {
-        this.analyser.getByteFrequencyData(this.dataArray);
-        let sum = 0;
-        for (let i = 0; i < this.dataArray.length; i++) {
-            sum += this.dataArray[i];
-        }
-        return sum / this.dataArray.length;        
-    }
+	_getEnergy () {
+		this.analyser.getByteFrequencyData(this.dataArray);
+		let sum = 0;
+		for (let i = 0; i < this.dataArray.length; i++) {
+			sum += this.dataArray[i];
+		}
+		return sum / this.dataArray.length;		
+	}
 
-    monitor() {
+	monitor() {
  
-     
+	 
 
-        if (this.getEnergy () < this.threshold * this.thresholdWeight ) {
-            // useful audio is not present
-            if (!this.isSilent) {
-                if (this.silenceTimeout === null) {
-                    const silenceWasAt = Date.now();
-                    this.silenceTimeout = setTimeout(() => {
-                        const silenceDuration = Date.now() - silenceWasAt;
-                        const audioDuration = this.lastAudioSeenAt ? silenceWasAt- this.lastAudioSeenAt : null;
-                        this.isSilent = true;
-                        this.silenceWasAt = silenceWasAt;
-                        this.emitEvent('silenceDetected', {silenceWasAt,audioDuration,silenceDuration,lastAudioSeenAt:this.lastAudioSeenAt});
-                    }, this.silenceDuration);
-                }
-            }
-        } else {
-            // useful audio is present
-            this.lastAudioSeenAt = Date.now();
-            if (this.isSilent) {
-                // audio just resumed after a period of silence
-                this.isSilent = false;
-                this.lastAudioNotified = lastAudioSeenAt;
-                this.audioResumedAt = this.lastAudioNotified;
-                this.previousSilenceDuration = this.silenceWasAt ?  this.audioResumedAt - this.silenceWasAt : null;
-                this.emitEvent('audioResumed', {audioResumedAt:this.audioResumedAt,previousSilenceDuration:this.previousSilenceDuration,silenceWasAt:this.silenceWasAt});
-            }
-            if (this.silenceTimeout !== null) {
-                clearTimeout(this.silenceTimeout);
-                this.silenceTimeout = null;
-            } else {
-                if ( (this.lastAudioSeenAt - this.lastAudioNotified) > this.renotifyTimeout ) {
-                    this.lastAudioNotified = this.lastAudioSeenAt;
-                    const audioDuration = this.audioResumedAt ? this.lastAudioSeenAt - this.audioResumedAt : null;
-                    this.emitEvent('audioActive', {audioResumedAt:this.audioResumedAt,audioDuration,previousSilenceDuration:this.previousSilenceDuration,silenceWasAt:this.silenceWasAt});
-                }
-            }
-        }
+		if (this.getEnergy () < this.threshold * this.thresholdWeight ) {
+			// useful audio is not present
+			if (!this.isSilent) {
+				if (this.silenceTimeout === null) {
+					const silenceWasAt = Date.now();
+					this.silenceTimeout = setTimeout(() => {
+						const silenceDuration = Date.now() - silenceWasAt;
+						const audioDuration = this.lastAudioSeenAt ? silenceWasAt- this.lastAudioSeenAt : null;
+						this.isSilent = true;
+						this.silenceWasAt = silenceWasAt;
+						this.emitEvent('silenceDetected', {silenceWasAt,audioDuration,silenceDuration,lastAudioSeenAt:this.lastAudioSeenAt});
+					}, this.silenceDuration);
+				}
+			}
+		} else {
+			// useful audio is present
+			this.lastAudioSeenAt = Date.now();
+			if (this.isSilent) {
+				// audio just resumed after a period of silence
+				this.isSilent = false;
+				this.lastAudioNotified = this.lastAudioSeenAt;
+				this.audioResumedAt = this.lastAudioNotified;
+				this    .previousSilenceDuration = this.silenceWasAt ?  this.audioResumedAt - this.silenceWasAt : null;
+				this.emitEvent('audioResumed', {audioResumedAt:this.audioResumedAt,previousSilenceDuration:this.previousSilenceDuration,silenceWasAt:this.silenceWasAt});
+			}
+			if (this.silenceTimeout !== null) {
+				clearTimeout(this.silenceTimeout);
+				this.silenceTimeout = null;
+			} else {
+				if ( (this.lastAudioSeenAt - this.lastAudioNotified) > this.renotifyTimeout ) {
+					this.lastAudioNotified = this.lastAudioSeenAt;
+					const audioDuration = this.audioResumedAt ? this.lastAudioSeenAt - this.audioResumedAt : null;
+					this.emitEvent('audioActive', {audioResumedAt:this.audioResumedAt,audioDuration,previousSilenceDuration:this.previousSilenceDuration,silenceWasAt:this.silenceWasAt});
+				}
+			}
+		}
 
-        requestAnimationFrame(() => this.monitor());
-    }
+		requestAnimationFrame(() => this.monitor());
+	}
 
-    emitEvent(eventType, timestamp) {       
-        const event = new CustomEvent(eventType, { detail: { timestamp } });  
-        window.dispatchEvent(event);
-        if (this.eventTarget && this.eventTarget.postMessage) {
-            this.eventTarget.postMessage({ event:eventType,detail: { timestamp }},"*");
-        }
+	emitEvent(eventType, timestamp) {	   
+		const event = new CustomEvent(eventType, { detail: { timestamp } });  
+		window.dispatchEvent(event);
+		if (this.eventTarget && this.eventTarget.postMessage) {
+			this.eventTarget.postMessage({ event:eventType,detail: { timestamp }},"*");
+		}
 
-        
-    }
+		
+	}
 
-    setThreshold(value,weight) {
-        this.threshold = value;
-        this.thresholdWeight  = weight || 256;
-    }
+	setThreshold(value,weight) {
+		this.threshold = value;
+		this.thresholdWeight  = weight || 256;
+	}
 
-    setEnergyFunc(getEnergyFunction) {
-        this.getEnergy = getEnergyFunction;
-    }
+	setEnergyFunc(getEnergyFunction) {
+		this.getEnergy = getEnergyFunction;
+	}
 }
