0b0df256908112a24aff75633834d8d1fa1377c6
Jonathan <jonathan.max.annett@gmail.com>
2024-08-01 12:38:23 +1000

    Update silenceDetect.js

--- mcec/speech/silenceDetect.js
+++ mcec/speech/silenceDetect.js
@@ -2,7 +2,7 @@
 //jshint esversion : 8
 //jshint undef : true
 
-class SilenceDetector {
+class SilenceDetector0 {
 	constructor(threshold = 0.01, silenceDuration = 5000, renotifyTimeout = 500, getEnergyFunction=null,target=null) {
 		this.eventTarget = (target !== window && target) || null;
 		this.threshold = threshold;
@@ -117,6 +117,125 @@ class SilenceDetector {
 	setThreshold(value,weight) {
 		this.threshold = value;
 		this.thresholdWeight  = weight || 256;
+	}
+
+	setEnergyFunc(getEnergyFunction) {
+		this.getEnergy = getEnergyFunction;
+	}
+}
+
+
+//jshint esnext : false
+//jshint esversion : 8
+//jshint undef : true
+
+class SilenceDetector {
+	constructor(threshold = 0.01, silenceDuration = 5000, renotifyTimeout = 500, getEnergyFunction = null, target = null) {
+		this.eventTarget = (target !== window && target) || null;
+		this.threshold = threshold;
+		this.thresholdWeight = 256;
+		this.silenceDuration = silenceDuration;
+		this.silenceTimeout = null;
+		this.isSilent = false;
+		this.renotifyTimeout = renotifyTimeout;
+		this.lastAudioNotified = Date.now();
+		this.firstCheck = true;
+		if (typeof getEnergyFunction === 'function') {
+			this.getEnergy = getEnergyFunction;
+			this.monitor();
+		} else {
+			const AC = window.AudioContext || window.webkitAudioContext;
+			this.audioContext = new AC();
+			this.getEnergy = this._getEnergy;
+			this.init();
+		}
+	}
+
+	async init() {
+		try {
+			const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+			this.audioInput = this.audioContext.createMediaStreamSource(stream);
+			this.analyser = this.audioContext.createAnalyser();
+			this.analyser.fftSize = 256;
+			this.dataArray = new Uint8Array(this.analyser.frequencyBinCount);
+			this.audioInput.connect(this.analyser);
+			this.initialCheck();
+		} catch (error) {
+			console.error('Error accessing audio input:', error);
+		}
+	}
+
+	_getEnergy() {
+		this.analyser.getByteFrequencyData(this.dataArray);
+		let sum = 0;
+		for (let i = 0; i < this.dataArray.length; i++) {
+			sum += this.dataArray[i];
+		}
+		return sum / this.dataArray.length;
+	}
+
+	initialCheck() {
+		const energy = this.getEnergy();
+		if (energy < this.threshold * this.thresholdWeight) {
+			this.isSilent = true;
+			this.emitEvent('silenceDetected', { silenceWasAt: Date.now(), audioDuration: 0, silenceDuration: 0 });
+		} else {
+			this.isSilent = false;
+			this.emitEvent('audioResumed', { audioResumedAt: Date.now(), previousSilenceDuration: 0, silenceWasAt: 0 });
+		}
+		this.monitor();
+	}
+
+	monitor() {
+		const energy = this.getEnergy();
+		const now = Date.now();
+
+		if (energy < this.threshold * this.thresholdWeight) {
+			if (!this.lastAudioSeenAt) {
+				this.lastAudioSeenAt = now;
+			}
+
+			if (!this.isSilent) {
+				if (this.silenceTimeout === null) {
+					this.silenceTimeout = setTimeout(() => {
+						const silenceDuration = now - this.lastAudioSeenAt;
+						this.isSilent = true;
+						this.emitEvent('silenceDetected', { silenceWasAt: now, audioDuration: now - this.lastAudioNotified, silenceDuration, lastAudioSeenAt: this.lastAudioSeenAt });
+					}, this.silenceDuration);
+				}
+			}
+		} else {
+			this.lastAudioSeenAt = now;
+
+			if (this.isSilent) {
+				this.isSilent = false;
+				this.lastAudioNotified = now;
+				this.emitEvent('audioResumed', { audioResumedAt: now, previousSilenceDuration: now - this.silenceWasAt, silenceWasAt: this.silenceWasAt });
+			}
+
+			if (this.silenceTimeout !== null) {
+				clearTimeout(this.silenceTimeout);
+				this.silenceTimeout = null;
+			} else if (now - this.lastAudioNotified > this.renotifyTimeout) {
+				this.lastAudioNotified = now;
+				this.emitEvent('audioActive', { audioResumedAt: this.lastAudioNotified, audioDuration: now - this.lastAudioNotified, previousSilenceDuration: now - this.silenceWasAt, silenceWasAt: this.silenceWasAt });
+			}
+		}
+
+		requestAnimationFrame(() => this.monitor());
+	}
+
+	emitEvent(eventType, detail) {
+		const event = new CustomEvent(eventType, { detail: detail });
+		window.dispatchEvent(event);
+		if (this.eventTarget && this.eventTarget.postMessage) {
+			this.eventTarget.postMessage({ event: eventType, detail }, "*");
+		}
+	}
+
+	setThreshold(value, weight) {
+		this.threshold = value;
+		this.thresholdWeight = weight || 256;
 	}
 
 	setEnergyFunc(getEnergyFunction) {
